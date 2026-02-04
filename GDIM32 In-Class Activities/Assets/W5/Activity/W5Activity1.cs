83124b75c567d1ec2656077287ad80f16109902f
gaelp-io <gandresp20@gmail.com>
2026-02-03 17:33:54 -0800

    fixed W5 activity script

--- GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
+++ GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
+
 
 public class W5Activity1 : MonoBehaviour
 {
@@ -58,8 +58,12 @@ public class Axe : Item, IBreakable {
 		Debug.Log("axe now has " + _durability + " durability remaining");
 		
 		if(_durability <= 0) {
-			Break();
+			 Break();
 		}
+	}
+
+	public void Break() {
+		Debug.Log("axe broke");
 	}
 	
 	public override void Use () {
@@ -69,13 +73,14 @@ public class Axe : Item, IBreakable {
 }
 
 public class ElvenSword : Item {
-	public void Use () {
+	public override void Use () {
 		Debug.Log("attacking with Elven sword");
 	}
 }
 
 public class Torch : Item, IBreakable {
-	
+	private float _durability = 1.0f;
+
 	public void Damage (float damage) {
 		_durability -= damage;
 		Debug.Log("torch now has " + _durability + " durability remaining");
@@ -85,14 +90,12 @@ public class Torch : Item, IBreakable {
 		}
 	}
 	
-	public void BreakItem () {
+	public void Break () {
 		Debug.Log("torch is broken!");
 	}
 	
-	protected override void Use () {
+	public override void Use () {
 		Debug.Log("lighting area with torch");
 		Damage(1.0f);
 	}
 }
-
-*/
